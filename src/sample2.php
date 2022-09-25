3094e336eaafb351ed4c84c28cd7412eaaa95c7f
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2022-09-25 11:48:31 +0900

    tweak sample

--- src/sample2.php
+++ src/sample2.php
@@ -1,15 +1,20 @@
 <?php
 $code = <<<'CODE'
 struct {
-    long foo;
-    double bar;
+    double foo;
+    long bar;
+    int baz;
 }
 CODE;
 
 $type = FFI::type($code);
 $data = FFI::new($type);
 FFI::memset($data, 0, FFI::sizeof($type));
-printf("foo: %d, bar: %f\n", $data->foo, $data->bar);
-$data->foo = PHP_INT_MAX;
-$data->bar = M_PI;
-printf("foo: %d, bar: %f\n", $data->foo, $data->bar);
+
+printf("foo: %f, bar: %d, baz: %d\n", $data->foo, $data->bar, $data->baz);
+
+$data->foo = M_PI;
+$data->bar = PHP_INT_MAX;
+$data->baz = PHP_INT_MAX;
+
+printf("foo: %f, bar: %d, baz: %d\n", $data->foo, $data->bar, $data->baz);
